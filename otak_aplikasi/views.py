f2de71a1d5b0f337e733b7f0b1872464de518a75
black-domon <jakfar.ilham23@gmail.com>
2026-01-12 13:45:48 +0700

    saya menambahkan halaman profil sekolah dengan menggunakan tema pada halaman dashboard

--- otak_aplikasi/views.py
+++ otak_aplikasi/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
-def index(request):
+def beranda(request):
     return render(request, 'index.html')
+
+def profil(request):
+    return render(request, 'profil_sekolah.html')
